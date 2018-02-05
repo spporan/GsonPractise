ad511d79d734a6709aa3c312c323dede5b364d8b
poran <mdshahparan520@gmail.com>
2018-02-06 02:13:46 +0600

    Deserialize

--- app/src/main/java/com/poran/gsonpractise/Main.java
+++ app/src/main/java/com/poran/gsonpractise/Main.java
@@ -9,7 +9,7 @@ import com.google.gson.Gson;
 public class Main {
 
     public static void main(String[]args){
-       // serialization();
+       serialization();
         deserialization();
 
     }
